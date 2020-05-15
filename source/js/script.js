aaeb33e12847b36a2c1b653126d1cde16267ac05
morrigan-mm <morrigan.87.mm@gmail.com>
2020-05-15 16:42:44 +0300

    Improve layout for PixelPerfect, add grafics

---
+++ source/js/script.js
@@ -0,0 +1,25 @@
+var header = document.querySelector(".page-header");
+var button = header.querySelector(".page-header__toggle");
+
+header.classList.toggle("page-header--collapsed");
+header.classList.remove("page-header--nojs");
+
+button.addEventListener("click", function () {
+  header.classList.toggle("page-header--collapsed");
+});
+
+var modal = document.querySelector(".modal");
+var overlay = document.querySelector(".modal__overlay");
+var modalOpenButtons = document.querySelectorAll(".button-modal-open-js");
+
+for (var i = 0; i < modalOpenButtons.length; i++) {
+  modalOpenButtons[i].addEventListener("click", function () {
+    modal.classList.add("modal--show");
+    document.body.style.overflow = "hidden";
+  });
+}
+
+overlay.addEventListener("click", function () {
+  modal.classList.remove("modal--show");
+  document.body.style.overflow = "";
+});
